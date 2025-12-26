9fa6d96b1da3169c98ba57d1e69ef53779cfc0f0
shubham singhk <shubhamsinghia160@gmail.com>
2025-12-26 23:44:56 +0530

    used axios and called api for user profile data but dues to 60 req/hr limit exeed i need to do oauth now in this , will do that next

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,35 @@
 
+import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
+  const [name, setName] = useState('')
+  const [result, setResult] = useState('');
+
+  // useEff
+  setName("shubhamili")
+  const getDetails = async () => {
+    const res = await axios.get(`https://api.github.com/users/${name}`)
 
 
+    setResult(res.data)
+  }
 
+  getDetails()
+  // const 
 
   return (
     <>
 
-      apis will show data here soon..
+      {
+        result ? <div> {result}</div> : <div> apis will show data here soon..</div>
+      }
 
 
 
+      {/* {(result) &&  apis will show data here soon..} */}
     </>
   )
 }
